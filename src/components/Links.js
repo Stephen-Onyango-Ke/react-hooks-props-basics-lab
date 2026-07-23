6d5143ae86d401ac9bf58288061cca0ebe54fbea
Steve <mac@MACs-MacBook-Pro.local>
2026-07-23 05:39:51 -0700

    first complete react app

---
+++ src/components/Links.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Links(){
+return(
+    <>
+     <h3>Links</h3>
+    <a href = "https://github.com/liza"> https://github.com/liza </a>
+    <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
+    
+    </>
+)
+
+}
+
+export default Links
+
+
+// an <h3> element with the text of "Links"
+// an <a> element with its href and text content set to URL of the user's Github link, passed down as a prop
+// a second <a> element with its href and text content set to URL of the user's LinkedIn link, passed down as a prop
+// Hint: make sure your <a> tags display the URL of the user's links as their text content so that it matches the demo image above
